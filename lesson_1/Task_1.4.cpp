0709f334bf48ded349afa93b6ae83494254809c5
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-12 18:27:59 +0200

    I fixed the error with calculator in lesson_1.4

--- lesson_1/Task_1.4.cpp
+++ lesson_1/Task_1.4.cpp
@@ -2,32 +2,28 @@
 using namespace std;
 int main()
 {
-	int number1, number2, number3;
+	int number1, number2, sol;
 	cout << "\t\t\t\t\t****calculator***\n";
 	cout << "Enter first number: ";
 	cin >> number1;
 	cout << "Enter second number: ";
 	cin >> number2;
-	cout << "Enter the third number: ";
-	cin >> number3;
-	if (number3 == 1)
+	cout << "What will you choose (+), (-), (*), (/): ";
+	cin >> sol;
+	if (sol == 1)
 	{
-		cout << number1 + number2;
+		cout <<"Result: "<< number1 + number2;
 	}
-	if (number3 == 2)
+	if (sol == 2)
 	{
-		cout << number1 - number2;
+		cout <<"Result: "<< number1 - number2;
 	}
-	if (number3 == 3)
+	if (sol == 3)
 	{
-		cout << number1 * number2;
+		cout <<"Result: "<< number1 * number2;
 	}
-	if (number3 == 4)
+	if (sol == 4)
 	{
-		cout << number1 / number2;
-	}
-	else
-	{
-		cout << "you entered an invalid number";
+		cout <<"Result: "<< number1 / number2;
 	}
 }
