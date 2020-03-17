a07f776b7dcc031ae83d3479ca1e4c32983725d7
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-17 16:15:06 +0200

    I was add total sum of cards

--- lesson_3/Task_3.2.cpp
+++ lesson_3/Task_3.2.cpp
@@ -4,13 +4,11 @@ using namespace std;
 
 int main()
 {
-    int cards[10], money = 0, num = 0;
-    
+    int cards[10], money = 0, num = 0, sum = 0;    
     for (int i = 0; i < 10; i++)
     {
         cards[i] = 0;
-        cout << cards[i] << " ";
-        
+        cout << cards[i] << " ";       
     }
     cout << "\n";
         while (1 > 0)
@@ -19,15 +17,18 @@ int main()
             cin >> num;
             cout << "How much money you want to put: ";
             cin >> money;
-            cards[num - 1] = cards[num - 1] + money;
-
+            cards[num - 1] = cards[num - 1] + money;          
             for (int b = 0; b < 10; b++)
-            {
-               
-                
-                cout << cards[b] << " ";
-                
+            {                             
+                cout << cards[b] << " ";               
             }
+            cout << "\n";
+            sum = 0;
+            for (int g = 0; g < 10; g++)
+            {
+                sum += cards[g];               
+            } 
+            cout << "Total sum of cards: " << sum;
             cout << "\n\n";
-        }  
+        }        
 }
