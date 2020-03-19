757b194f9c6376e54cc8ba41276be8900ec4efc1
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-19 20:04:01 +0200

    I completed a task_3.4 with column star

---
+++ lesson_3/Task_3.4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num[5], row = 0, i = 0, max = 0, cou = 0;    
+    while (i < 5)
+    {
+            cout << "Zvezdochek "<< i + 1 << " -om stolbike: ";
+            cin >> num[i];
+            i++;            
+    }
+    while (cou < 5)
+    {
+        if (max < num[cou])
+        {
+            max = num[cou];
+        }
+        cou++;
+    }
+        cou = 0;
+        while (cou < max)
+        {
+            row = 0;
+            while (row < 5)
+            {
+                if (cou < num[row])
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+                row++;
+            }
+            cout << endl;
+            cou++;
+        }       
+        cout << "\n";   
+}
