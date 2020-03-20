a05eb5125ec4cbc2768ea380830e1f84c904187c
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-20 14:42:14 +0200

    Task conclusion amount words in string

---
+++ lesson_4/Task_4.1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char array[100];
+	int wor = 0, sym = 0;
+	bool space = true;
+
+	cout << "Enter string: ";
+	cin.getline(array, 100);
+	while (array[wor] != 0)
+	{
+		if (array[wor] == ' ')
+		{
+			space = true;
+		}
+		else if (space)
+		{
+			sym++;
+			space = false;
+		}
+		wor++;
+	}
+	cout << "Words in string: " << sym << endl;
+}
