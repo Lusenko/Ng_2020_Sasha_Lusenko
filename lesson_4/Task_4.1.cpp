ce06e3f405d4222a41e109970755ec522ca77b0f
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-23 17:05:04 +0200

    Fixed task_4.1 and now the program ignores numbers

--- lesson_4/Task_4.1.cpp
+++ lesson_4/Task_4.1.cpp
@@ -5,12 +5,12 @@ int main()
 	char array[100];
 	int wor = 0, sym = 0;
 	bool space = true;
-
+	
 	cout << "Enter string: ";
 	cin.getline(array, 100);
 	while (array[wor] != 0)
 	{
-		if (array[wor] == ' ')
+		if (!(array[wor] >= 'a') && (array[wor] <= 'z') || (array[wor] >= 'A') && (array[wor] <= 'Z'))
 		{
 			space = true;
 		}
@@ -19,7 +19,7 @@ int main()
 			sym++;
 			space = false;
 		}
-		wor++;
-	}
+		wor++;		
+	}			
 	cout << "Words in string: " << sym << endl;
 }
