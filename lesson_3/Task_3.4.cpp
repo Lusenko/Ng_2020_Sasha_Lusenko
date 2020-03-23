b3dce38c8ef832f5bbf64e584de4e8e7941272ab
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-23 14:45:55 +0200

    I changed the variable and combined input and search

--- lesson_3/Task_3.4.cpp
+++ lesson_3/Task_3.4.cpp
@@ -4,39 +4,37 @@ using namespace std;
 
 int main()
 {
-    int num[5], row = 0, i = 0, max = 0, cou = 0;    
-    while (i < 5)
+    int num[5], row = 0, index = 0, max = 0, cou = 0;
+
+    while (index < 5)
     {
-            cout << "Zvezdochek "<< i + 1 << " -om stolbike: ";
-            cin >> num[i];
-            i++;            
-    }
-    while (cou < 5)
-    {
+        cout << "Zvezdochek " << index + 1 << " -om stolbike: ";
+        cin >> num[index];
+        index++;
         if (max < num[cou])
         {
             max = num[cou];
         }
         cou++;
     }
-        cou = 0;
-        while (cou < max)
+    cou = 0;
+    while (cou < max)
+    {
+        row = 0;
+        while (row < 5)
         {
-            row = 0;
-            while (row < 5)
+            if (cou < num[row])
             {
-                if (cou < num[row])
-                {
-                    cout << "*";
-                }
-                else
-                {
-                    cout << " ";
-                }
-                row++;
+                cout << "*";
             }
-            cout << endl;
-            cou++;
-        }       
-        cout << "\n";   
+            else
+            {
+                cout << " ";
+            }
+            row++;            
+        }
+        cout << "\n";
+        cou++;
+    }
+    cout << "\n";
 }
