fddf11a749bcfb0f816d29acae4976bb3955323d
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-27 14:21:37 +0200

    I added function for second number

--- lesson_5/Task_5.3.cpp
+++ lesson_5/Task_5.3.cpp
@@ -9,46 +9,38 @@ int Enter_number()
 	cin >> value;
 	return value;
 }
-int Sum(int number_1)
+int Num()
 {
-	int number_2;
 	cout << "Enter the second number: ";
-	cin >> number_2;
+	int sec;
+	cin >> sec;
+	return sec;
+}
+int Sum(int number_1, int number_2)
+{	
 	return number_1 + number_2;
 }
-int Res(int number_1)
+int Res(int number_1, int number_2)
 {	
-	int number_2;
-	cout << "Enter the second number: ";
-	cin >> number_2;
 	return number_1 - number_2;
 }
-int Mul(int number_1)
+int Mul(int number_1, int number_2)
 {	
-	int number_2;
-	cout << "Enter the second number: ";
-	cin >> number_2;
 	return number_1 * number_2;
 }
-int Div(int number_1)
+int Div(int number_1, int number_2)
 {	
-	int number_2;
-	cout << "Enter the second number: ";
-	cin >> number_2;
 	return number_1 / number_2;
 }
-int Sqrt(int number_1)
+int Sqrt(int number_1, int number_2)
 {
 	return sqrt(number_1);
 }
-int Power(int number_1)
+int Power(int number_1, int number_2)
 {
-	int power = 1;
-	int number_2;
-	int result = number_1;
-	cout << "Enter the second number: ";
-	cin >> number_2;
-    
+	int power = 1;	
+	int result = number_1;	
+
 	while (power < number_2)
 	{
 		result *= number_1;
@@ -61,18 +53,19 @@ int main()
 {
 	int sum = 0;
 	int value1 = Enter_number();
+	int sec = Num();
 	cout << "1(+) 2(-) 3(*) 4(/) 5(sqrt) 6(pow): ";	
 	cin >> sum;
 	switch (sum)
 	{
-		case 1: cout << "Result: " << Sum(value1) <<"\n"; break;
-		case 2: cout << "Result: " << Res(value1) << "\n"; break;
-		case 3: cout << "Result: " << Mul(value1) << "\n"; break;
-		case 4: cout << "Result: " << Div(value1) << "\n"; break;
-		case 5: cout << "Result: " << Sqrt(value1) << "\n"; break;
-		case 6: cout << "Result: " << Power(value1) << "\n"; break;
-		default:
-			cout << "Error!!!";
-			break;			
+	case 1: cout << "Result: " << Sum(value1,sec) << "\n"; break;
+	case 2: cout << "Result: " << Res(value1,sec) << "\n"; break;
+	case 3: cout << "Result: " << Mul(value1,sec) << "\n"; break;
+	case 4: cout << "Result: " << Div(value1,sec) << "\n"; break;
+	case 5: cout << "Result: " << Sqrt(value1,sec) << "\n"; break;
+	case 6: cout << "Result: " << Power(value1,sec) << "\n"; break;
+	default:
+		cout << "Error!!!";
+		break;
 	}
 }
