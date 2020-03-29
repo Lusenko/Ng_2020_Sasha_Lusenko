0b7ab09842216ad54f43bb22060bdf5145ea67e1
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-03-29 16:46:34 +0300

    I made a calculator through switch

--- lesson_1/Task_1.4.cpp
+++ lesson_1/Task_1.4.cpp
@@ -1,40 +1,24 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
 int main()
 {
-    int num[5], row = 0, i = 0, max = 0, cou = 0;
-    while (i < 5)
-    {
-        cout << "Zvezdochek " << i + 1 << " -om stolbike: ";
-        cin >> num[i];
-        i++;
-        if (max < num[cou])
-        {
-            max = num[cou];
-        }
-        cou++;
-    }
-    
-    cou = 0;
-    while (cou < max)
-    {
-        row = 0;
-        while (row < 5)
-        {
-            if (cou < num[row])
-            {
-                cout << "*";
-            }
-            else
-            {
-                cout << " ";
-            }
-            row++;
-        }
-        cout << endl;
-        cou++;
-    }
-    cout << "\n";
+	int num1, num2, sum;
+	cout << "\t\t\t\t\t\tColculator\n";
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
+	cout << "What you want to do 1(+) 2(-) 3(*) 4(/): ";
+	cin >> sum;
+
+	switch (sum)
+	{
+		case 1: cout << "Result: " << num1 + num2; break;
+		case 2: cout << "Result: " << num1 - num2; break;
+		case 3: cout << "Result: " << num1 * num2; break;
+		case 4: cout << "Result: " << num1 / num2; break;
+		default:
+			cout << "Error!!!";
+			break;
+	}
 }
