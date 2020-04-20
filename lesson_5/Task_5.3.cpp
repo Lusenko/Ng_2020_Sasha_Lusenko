0603e40f2d028d13cf1afda00e3e9454fa992b74
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2020-04-20 21:16:54 +0300

    I fixed calculator and redid functions

--- lesson_5/Task_5.3.cpp
+++ lesson_5/Task_5.3.cpp
@@ -2,19 +2,12 @@
 #include<cmath>
 using namespace std;
 
-int Enter_number()
+int Enter_number(int num)
 {
-	cout << "Enter the first number: ";
+	cout << "Enter the " << num << " number: ";
 	int value;
 	cin >> value;
 	return value;
-}
-int getNum()
-{
-	cout << "Enter the second number: ";
-	int sec;
-	cin >> sec;
-	return sec;
 }
 int getSum(int number_1, int number_2)
 {
@@ -38,35 +31,41 @@ double getSqrt(double number_1)
 }
 int getPower(int number_1, int number_2)
 {
-	int power = 1;
-	int result = number_1;
 
-	while (power < number_2)
+	int result = number_2;
+	for (int index = 1; index < number_1; index++)
 	{
-		result *= number_1;
-		power++;
+		result *= number_2;
 	}
 	return result;
 }
+int getAction()
+{
+	cout << "Select action: 1(+) 2(-) 3(*) 4(/) 5(sqrt) 6(pow): ";
+	int action;
+	cin >> action;
+	return action;
+}
+void processSwitch(int action, int value1, int value2)
+{
+	switch (action)
+	{
+	case 1: cout << "Result: " << getSum(value1, value2) << "\n\n"; break;
+	case 2: cout << "Result: " << getRes(value1, value2) << "\n\n"; break;
+	case 3: cout << "Result: " << getMul(value1, value2) << "\n\n"; break;
+	case 4: cout << "Result: " << getDiv(value1, value2) << "\n\n"; break;
+	case 5: cout << "Result: " << getSqrt(value1) << "\n\n"; break;
+	case 6: cout << "Result: " << getPower(value1, value2) << "\n\n"; break;
+	default: cout << "Error!!!"; break;
+	}
+}
 int main()
 {
-	while (1 > 0)
+	while (1)
 	{
-		int sum = 0;		
-		cout << "1(+) 2(-) 3(*) 4(/) 5(sqrt) 6(pow): ";		
-		cin >> sum;
-		switch (sum)
-		{
-			case 1: cout << "Result: " << getSum(Enter_number(),getNum()) << "\n"; break;
-			case 2: cout << "Result: " << getRes(Enter_number(), getNum()) << "\n"; break;
-			case 3: cout << "Result: " << getMul(Enter_number(), getNum()) << "\n"; break;
-			case 4: cout << "Result: " << getDiv(Enter_number(), getNum()) << "\n"; break;
-			case 5: cout << "Result: " << getSqrt(Enter_number()) << "\n"; break;
-			case 6: cout << "Result: " << getPower(Enter_number(), getNum()) << "\n"; break;
-			default:
-				cout << "Error!!!";
-				break;
-		}
-		cout << endl;
+		int action = 0;
+		int value1 = Enter_number(1);
+		int value2 = Enter_number(2);
+		processSwitch(getAction(), value1, value2);
 	}	
 }
